f8f31831545f70d25ab9f086b4718c3048ede6d7
mikapersson <perssonmika@gmail.com>
2020-04-21 20:08:33 +0200

    ex 1 finished

--- ComputerExercise/numericalex1.m
+++ ComputerExercise/numericalex1.m
@@ -4,7 +4,7 @@ clear all
 % SOLVE 1-D HEAT FLOW PROBLEM
 A=10; Q = 100; k=5; q = 15;   % given in the problem
 xStart = 2; xEnd = 8;         % where the object starts and ends
-nelm=3; nen=2; ndof= nelm+1;  % #elements, #endpoints/element, #degoffreed.
+nelm=20; nen=2; ndof= nelm+1;  % #elements, #endpoints/element, #degoffreed.
 
 x = linspace(xStart, xEnd, ndof);  % divide the object
 L = (x(end)-x(1))/nelm;            % length of each element
@@ -18,7 +18,7 @@ for elnr=1:nelm
 end
 
 K=zeros(ndof);
-F = zeros(ndof,1); F(xEnd) = -q*A;
+F = zeros(ndof,1); F(end) = -q*A;
 Fe = [Q*L/2 ; Q*L/2];
 
 for elnr = 1:nelm
@@ -27,3 +27,11 @@ end
 
 bc = [1 0];
 T = solveq(K,F,bc);
+
+% 3 elements: T = [0 14 20 18]'
+% 20 elements: T = [0;2.61000000000000;5.04000000000000;7.29000000000001;
+% 9.36000000000001;11.2500000000000;12.9600000000000;14.4900000000000;
+% 15.8400000000000;17.0100000000000;18.0000000000000;18.8100000000000;
+% 19.4400000000000;19.8900000000000;20.1600000000001;20.2500000000001;
+% 20.1600000000000;19.8900000000000;19.4400000000000;18.8100000000000;
+% 18.0000000000000]
