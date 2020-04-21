b88a7c112ff5117d244a3de716cfb360c2ada070
mikapersson <perssonmika@gmail.com>
2020-04-21 18:54:14 +0200

    working on example1

--- ComputerExercise/numericalex1.m
+++ ComputerExercise/numericalex1.m
@@ -2,9 +2,11 @@ close all
 clear all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Preprocessor
-A=10;
-E=1;
-L=1;
+A=10; Q = 100;
+k=1;
+xStart = 2; xEnd = 8;
+nelm=3; nen=2;
+ndof=8;
 
 Coord=[ -L 0  % coordinates for the nodes
 -L/sqrt(2) L/sqrt(2)
@@ -15,9 +17,8 @@ Edof=[1 1 2 5 6
 2 3 4 5 6
 3 5 6 7 8];
 
-nen=2;
-ndof=8;
-nelm=3;
+
+
 
 Dof=[ 1 2
 3 4
