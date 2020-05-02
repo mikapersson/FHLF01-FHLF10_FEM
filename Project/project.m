fcac77ff16f1ca0544f86127bcbe602691b9eca5
mikapersson <perssonmika@gmail.com>
2020-05-02 14:45:05 +0200

    extracted coordinates

--- Project/project.m
+++ Project/project.m
@@ -21,10 +21,13 @@ nelm=size(enod,1);                  % number of elements
 nnod=size(coord,1);                 % number of nodes
 dof=(1:nnod)';                      % dof number is node number
 dof_S=[(1:nnod)',(nnod+1:2*nnod)']; % give each dof a number
+
 for ie=1:nelm
     edof_S(ie,:)=[ie dof_S(enod(ie,1),:), dof_S(enod(ie,2),:),dof_S(enod(ie,3),:)];
     edof(ie,:)=[ie,enod(ie,:)];
 end
+
+[ex,ey]=coordxtr(edof,coord,dof,3);  % x- and y coordinates for each element
 
 % Check which segments that should have convections
 er = e([1 2 5],:); % Reduced e (only interested of rows 1, 2 and 5)
