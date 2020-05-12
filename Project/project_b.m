d6f94db6c1c75643b09e0f13761c863558909171
mikapersson <perssonmika@gmail.com>
2020-05-12 17:06:07 +0200

    working on b)

--- Project/project_b.m
+++ Project/project_b.m
@@ -6,7 +6,7 @@
 
 C = zeros(ndof);
 for elnr=1:nelm
-    subdomain = t(4,elnr);         % what subdomain the current element belongs to
+    subdomain = t(4,elnr);  % what subdomain the current element belongs to
     x = null(1);
     switch subdomain
     case 1
@@ -25,6 +25,40 @@ for elnr=1:nelm
 end
 
 time_step = 1;  % step i time integration
+
+transient_T = zeros(size(T,1),20);  % node temperatures for each time point
+temp_T = init_Temp*ones(size(T));
+transient_T(:,1) = temp_T;  % node temperatures for each time point
+
+% Calculate node temperatures for each time step
+
+temp_f = 
+for t=2:20
+    temp_T = (C+time_step*K)\(C*temp_T + time_step * temp_f);
+    trainsient_T(t,1) = temp_T;
+end
+
+
+eT=extract(edof,T);   % element temperatures
+
+% In order to plot both sides of the symmetry cut:
+patch(ex',ey',eT','EdgeColor','none');   %
+hold on     
+patch(-ex',ey',eT','EdgeColor','none');  % 
+
+title('Temperature distribution [C]')
+colormap(hot);
+colorbar;
+xlabel('x-position [m]')
+ylabel('y-position [m]')
+axis equal
+    
+
+
+
+
+
+
 
 
 
